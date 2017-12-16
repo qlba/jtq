f1861b8d0802bbe2ff543670955e0dcdf2f01e01
qlba <qlba@ya.ru>
2017-12-16 12:33:29 +0300

    Add project files.

---
+++ polunin_kursach/Matrix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polunin_kursach
+{
+    public class Matrix<T>
+    {
+        private T[,] data;
+
+        public Matrix(int n, int m)
+        {
+            data = new T[n, m];
+        }
+
+        //public T[](int i, int j)
+        //    {
+        //    }
+
+        public static T operator+(Matrix<T> lhs, Matrix<T> rhs)
+        {
+
+        }
+    }
+}
